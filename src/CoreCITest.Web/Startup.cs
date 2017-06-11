dbd097ed5c8a5c92669bab202c82108ecb590a26
Blake Phillips <phillips.blakej@gmail.com>
2017-06-11 14:54:15 -0700

    restructured a bit

--- src/CoreCITest.Web/Startup.cs
+++ src/CoreCITest.Web/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace dotnet
+namespace CoreCITest.Web
 {
     public class Startup
     {
