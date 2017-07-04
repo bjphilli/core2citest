f339633f85158240037f8bc13580faa2344b698b
Blake Phillips <phillips.blakej@gmail.com>
2017-07-03 21:35:35 -0700

    fix the build

--- src/CoreCITest.Web/Controllers/HomeController.cs
+++ src/CoreCITest.Web/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using CoreCITest.Web.Models;
 using CoreCITest.Data;
 
 namespace CoreCITest.Web.Controllers
